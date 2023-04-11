b30c6dcca81f8e769ed070de25c69456a5ee3bc7
yuliyaparamonava <yuliyaparamonava@localhost>
2023-04-11 19:00:24 -0400

    Yuliya added location method

---
+++ src/yuliya/paramonava/YuliyaClass.java
@@ -0,0 +1,20 @@
+package yuliya.paramonava;
+
+public class YuliyaClass {
+	
+	public static void main (String args []) {
+		
+		
+		System.out.println("This is Yuliya's main method...");
+		
+	}
+	
+	public void getInfo() {
+		
+		System.out.println("We are practicing with the team...");
+	}
+
+	public void location() {
+		System.out.println("I am in Arizona, where are u guys?");
+	}
+}
