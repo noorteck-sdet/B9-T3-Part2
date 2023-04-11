eeeccf04530f1ed2e55b1fba8fceeb2465c93cc5
juanv <juanv@DESKTOP-TT7GUJN>
2023-04-11 14:17:12 -0400

    Juan First Commit

---
+++ src/concepcionJuan/JuanConcepcion.java
@@ -0,0 +1,14 @@
+package concepcionJuan;
+
+public class JuanConcepcion {
+
+	public void juan(String a) {
+		a = "Juan";
+		
+	}
+	
+	public static void concepcion(String b) {
+		b = "Concepcion";
+		
+	}
+}
