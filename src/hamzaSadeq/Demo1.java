773e8fdd1ba806c36889e567ea7395a37a1119a3
hamzasadeq <hamzasadeq@10.0.0.7>
2023-04-11 17:08:17 -0400

    Testing

---
+++ src/hamzaSadeq/Demo1.java
@@ -0,0 +1,19 @@
+package hamzaSadeq;
+
+public class Demo1 {
+
+	public static void main(String[] arge) {
+
+	}
+
+	public static void setName() {
+
+		System.out.println("Hamza");
+	}
+
+	public static void getInfo() {
+
+		System.out.println("NoorTeck");
+	}
+
+}
