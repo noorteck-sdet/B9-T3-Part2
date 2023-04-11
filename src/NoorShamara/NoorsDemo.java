6065ed4de5f1fb85d3706247c66a975b7d1fdc0a
noors <noors@Noor>
2023-04-10 23:46:36 -0400

    my package

---
+++ src/NoorShamara/NoorsDemo.java
@@ -0,0 +1,17 @@
+package NoorShamara;
+
+public class NoorsDemo {
+
+	public static void main(String[] args) {
+	
+
+	}
+	public static void setName(){
+		System.out.println("Noor");
+		
+	}
+	public static void setAge() {
+		System.out.println("10");
+	}
+
+}
