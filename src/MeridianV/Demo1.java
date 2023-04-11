1bd87b1614f3b669103a642bfa29374f01cef746
Office <Office@DESKTOP-8MQLQAH.fios-router.home>
2023-04-11 11:56:24 -0400

    Added Meridian Package

---
+++ src/MeridianV/Demo1.java
@@ -0,0 +1,22 @@
+package MeridianV;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		test();
+		SetStudentName();		
+		
+	}
+
+	public static void test () {
+		
+		System.out.println("First push test for B9-T3-Part2....");
+	}
+	
+	public static void SetStudentName () {
+		System.out.println("Meridian Valverde");
+	
+	}
+
+}
